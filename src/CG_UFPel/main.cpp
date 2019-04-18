793ed10f87b8d71782f5dde144cb57a5d2ba05cc
gabselbach <gsstaniecki@inf.ufpel.edu.br>
2019-04-18 20:55:13 -0300

    implementação de lookat,e tempo

--- src/CG_UFPel/main.cpp
+++ src/CG_UFPel/main.cpp
@@ -30,7 +30,6 @@ bool firstMouse = true;
 // timing
 float deltaTime = 0.0f;
 float lastFrame = 0.0f;
-float firstTime = 0.0f;
 float distancia = 0.2f;
 float temp = 2.00f;
 float x = 0.0f;
@@ -84,25 +83,35 @@ int main()
 	// load models
 	// -----------
 	Model ourModel(FileSystem::getPath("resources/objects/nanosuit/nanosuit.obj"));
+	Model planet(FileSystem::getPath("resources/objects/planet/planet.obj"));
+	//Model rock(FileSystem::getPath("resources/objects/rock/rock.obj"));
+	//Model cyborg(FileSystem::getPath("resources/objects/cyborg/cyborg.obj"));
 	//Model novo(FileSystem::getPath("resources/objects/rock/rock.obj"));
 
-
+	/*vector<Model> models;
+	int modelcorrent=0;
+	models.push_back(nano);
+	models.push_back(planet);
+	models.push_back(rock);
+	models.push_back(cyborg);*/
 		// render loop
-	double atualTime=0.0;
-	double firstTime =0.0;
+	const double duracao =3.0;
 	glm::mat4 model;
-	glm::vec3 inicial = glm::vec3(-2.0f, -1.0f, 0.0f);
+	glm::vec3 inicial = glm::vec3(-20.37f,0.0f,-1.0f);
     glm::vec3 einicial = glm::vec3(0.1f, 0.1f, 0.1f);
-	//parâmetros da curva
-	double delt;
-	//vector<Model> models;
+	
+	//parametros de time
+	double delta_Time=0.0;
+	double atualTime=0.0;
+	double first_Time =0.0;
+	int n,aux=1;
+	glm::mat4 view;
+	bool playAnimationLinear = false;
 	while (!glfwWindowShouldClose(window))
 	{
-		//glm::vec3 pont = ourModel.calculoponto();
-		//camera.angulocamera(pont);
-		
-
+	
 		ourModel.calculoDelta();
+		//models[modelcorrent].calculoDelta();
 
 		//wireframe
 		if (glfwGetKey(window, GLFW_KEY_P) == GLFW_PRESS)
@@ -110,30 +119,53 @@ int main()
 		if (glfwGetKey(window, GLFW_KEY_U) == GLFW_PRESS)
 			glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
 
+		/*troca de modelo
+		if ((glfwGetKey(window, GLFW_KEY_ENTER) == GLFW_PRESS)) {
+			if(aux<4)
+				++aux;
+			if ((modelcorrent + 1) >= models.size())
+				modelcorrent = 0;
+			else
+				++modelcorrent;
+		}*/
 		//adicionaobjeto
-		if (glfwGetKey(window, GLFW_KEY_R) == GLFW_PRESS && ourModel.retornaDelta() > 0.5f)
-		{
-			ourModel.adicionaObjeto();
-	
+		if (glfwGetKey(window, GLFW_KEY_R) == GLFW_PRESS && ourModel.retornaDelta() > 0.5f){
+			 ourModel.adicionaObjeto();
+			
 		}
+
+	
 		//rodar por ponto
 		
 		if (glfwGetKey(window, GLFW_KEY_V) == GLFW_PRESS) {
 			glm::vec3 p(-2.5f,1.0f,-2.0f);
 			ourModel.roda(10,p,window,ourShader);
+			//models[modelcorrent].roda(10,p,window,ourShader);
 		}
-		//troca de modelos
-		if (glfwGetKey(window, GLFW_KEY_I) == GLFW_PRESS && ourModel.retornaDelta() > 0.5f){
+		//troca de objeto
+		if (glfwGetKey(window, GLFW_KEY_I) == GLFW_PRESS && ourModel.retornaDelta() > 0.5f)
 			ourModel.trocaObjetoMax();
-		}
-		if (glfwGetKey(window, GLFW_KEY_B) == GLFW_PRESS && ourModel.retornaDelta() > 0.5f){
+			//models[modelcorrent].trocaObjetoMax();
+		if (glfwGetKey(window, GLFW_KEY_B) == GLFW_PRESS && ourModel.retornaDelta() > 0.5f)
 			ourModel.trocaObjetoMin();
-		}
+			//models[modelcorrent].trocaObjetoMin();
 		//Animação translação
-		if (glfwGetKey(window, GLFW_KEY_M) == GLFW_PRESS) {
-			ourModel.translat(3,ourShader,window);
-
+		if (glfwGetKey(window, GLFW_KEY_M) == GLFW_PRESS){
+			playAnimationLinear = true;
+			first_Time = glfwGetTime();
 		}
+		if(playAnimationLinear){
+			if(delta_Time<duracao){
+				delta_Time = glfwGetTime() - first_Time;
+					ourModel.translat(delta_Time,ourShader,window);		
+			}else{
+				playAnimationLinear = false;
+				delta_Time=0.0;
+			}
+		}
+			//ourModel.translat(3,ourShader,window);
+			//models[modelcorrent].translat(3,ourShader,window);
+
 		//rodaobjetoespecifico
 		if (glfwGetKey(window, GLFW_KEY_F) == GLFW_PRESS){
 			//ourModel.rodaobjetoespecifico();
@@ -142,17 +174,24 @@ int main()
 		if (glfwGetKey(window, GLFW_KEY_G) == GLFW_PRESS){
 			glm::vec3 ponto(0.5f, 0.5f, 0.5f);
 			ourModel.rodaemponto(ponto);
+			//models[modelcorrent].rodaemponto(ponto);
 		}
-		if (glfwGetKey(window, GLFW_KEY_N) == GLFW_PRESS){
+		if (glfwGetKey(window, GLFW_KEY_N) == GLFW_PRESS)
 			ourModel.bezier();
-		}
+			//models[modelcorrent].bezier();
 		//escala
-		if (glfwGetKey(window, GLFW_KEY_C) == GLFW_PRESS){
-			ourModel.escala(window,ourShader,0.8f,1);
-		}
-		if(glfwGetKey(window, GLFW_KEY_T) == GLFW_PRESS && ourModel.retornaDelta() > 0.5f){
+		if (glfwGetKey(window, GLFW_KEY_C) == GLFW_PRESS && ourModel.retornaDelta() > 0.5f)
+			ourModel.escala(1.3f,1);
+			//models[modelcorrent].escala(window,ourShader,0.8f,1);
+		if(glfwGetKey(window, GLFW_KEY_T) == GLFW_PRESS && ourModel.retornaDelta() > 0.5f)
 			ourModel.rodanoeixo(10,window,ourShader,1);
+			//models[modelcorrent].rodanoeixo(10,window,ourShader,1);
+		if (glfwGetKey(window, GLFW_KEY_9) == GLFW_PRESS){
+			glm::vec3 novaposition = ourModel.retornaPositionObj();
+			camera.trocaFront(novaposition);
 		}
+			
+		
 
 		// per-frame time logic
 		// --------------------
@@ -160,32 +199,45 @@ int main()
 		deltaTime = currentFrame - lastFrame;
 		lastFrame = currentFrame;
 
-
-		//model = glm::translate(model, inicial); // translate it down so it's at the center of the scene
-       // model = glm::scale(model,einicial);
-		//ourShader.setMat4("model", model);
-		//novo.Draw(ourShader);
-		// input22
-		// -----
 		processInput(window);
 
 		// render
 		// ------
+		model = glm::translate(glm::scale(glm::mat4(1.0f), glm::vec3(0.1f,0.1f,0.1f)), inicial);
 		glClearColor(0.05f, 0.05f, 0.05f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
 		// don't forget to enable shader before setting uniforms
 		ourShader.use();
-
+		float radius = 10.0f;
+		float camX = sin(glfwGetTime()) * radius;
+		float camZ = cos(glfwGetTime()) * radius;
+		view = camera.GetViewMatrix();
+		if(glfwGetKey(window, GLFW_KEY_5) == GLFW_PRESS )
+			camera.olhaPonto(glm::vec3( -20.37f,0.0f,0.0f));
+		//olhar para o ponto
+		if(glfwGetKey(window, GLFW_KEY_4) == GLFW_PRESS )
+			 view = glm::lookAt( camera.Position,glm::vec3(-2.0f,0.0f,0.0f), glm::vec3(0.0f, 1.0f,0.0f));
 		// view/projection transformations
-		glm::mat4 projection = glm::perspective(glm::radians(camera.Zoom), (float)SCR_WIDTH / (float)SCR_HEIGHT, 0.1f, 100.0f);
-		glm::mat4 view = camera.GetViewMatrix();
+		if(glfwGetKey(window, GLFW_KEY_7) == GLFW_PRESS ){
+			ourModel.o();
+
+		}
+		 std::cout<<"new "<< std::endl;
+		glm::mat4 projection = glm::perspective(glm::radians(camera.Zoom), (float)SCR_WIDTH / (float)SCR_HEIGHT, 0.8f, 100.0f);
 		ourShader.setMat4("projection", projection);
 		ourShader.setMat4("view", view);
+		ourShader.setMat4("model", model);
+		planet.Draw(ourShader);
 		
 
-		ourModel.desenha(ourShader);
 		
+		/*for (  n= 0; n <aux; ++n)
+		{
+			models[n].desenha(ourShader);
+		}
+		*/
+		ourModel.desenha(ourShader);
 		// glfw: swap buffers and poll IO events (keys pressed/released, mouse moved etc.)
 		// -------------------------------------------------------------------------------
 		glfwSwapBuffers(window);
